3519ee1c4899beb8a594ac1078d4005821f865bd
Nicolo Maioli <nicolomaioli@gmail.com>
2023-08-01 12:54:02 +0100

    Use app router

---
+++ .prettierrc.cjs
@@ -0,0 +1,13 @@
+/** @type {import("prettier").Config} */
+const config = {
+  arrowParens: 'always',
+  printWidth: 80,
+  proseWrap: 'always',
+  singleQuote: true,
+  jsxSingleQuote: false,
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+};
+
+module.exports = config;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <h1>Hello World</h1>
+      </div>
+    </main>
+  )
+}
