3b32e4d1073a253baf4d75bb87b1a349c50a0fb5
Nicolo Maioli <nicolomaioli@gmail.com>
2024-02-08 14:43:06 +0000

    Update deps, add tailwind, remove env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import styles from './page.module.css'
-
-export default function Home() {
+const Page = () => {
   return (
-    <main>
-      <div>
+    <main className="container mx-auto">
+      <div className="prose">
         <h1>Hello World</h1>
       </div>
     </main>
-  )
-}
+  );
+};
+
+export default Page;
